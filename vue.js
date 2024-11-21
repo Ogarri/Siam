ab5ff7561f32c3035dd9a24eda42049ccc210e2d
Ogarri <143884627+Ogarri@users.noreply.github.com>
2024-11-21 21:46:03 +0100

    Ajout des cailloux

--- vue.js
+++ vue.js
@@ -19,11 +19,20 @@ document.addEventListener('DOMContentLoaded', function() {
 
   for (let x = 0; x < nb_col; x++) {
     var x_case = document.getElementById(`case-0-${x}`);
-    x_case.style.backgroundImage = `url('assets/rouge${x}.png')`;
+    x_case.style.backgroundImage = `url('assets/Rouge${x}.png')`;
   }
   for (let y = 0; y < nb_col; y++) {
     var y_case = document.getElementById(`case-6-${y}`);
-    y_case.style.backgroundImage = `url('assets/bleu${y}.png')`;
+    y_case.style.backgroundImage = `url('assets/Bleu${y}.png')`;
   }
 
+  for (let i = 1; i < 4; i++) {
+    var k = aleatoire(1, 4);
+    var caillou_case = document.getElementById(`case-3-${i}`);
+    caillou_case.style.backgroundImage = `url('assets/Caillou${k}.png')`;
+  }
 });
+
+function aleatoire (a, b) {
+  return Math.round(Math.random() * (b - a) + a)
+}
