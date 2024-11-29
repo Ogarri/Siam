fb4c9d3dd286fccb46fb6970deaa88183bb2dad2
Ogarri <ba.morlion@gmail.com>
2024-11-29 10:20:39 +0100

    moddification du modele

--- modele.js
+++ modele.js
@@ -1,1 +1,28 @@
 console.log("modele chargé");
+
+class Pion {
+    constructor(couleur) {
+        this.couleur = couleur;
+    }
+}
+
+class Tableau {
+    constructor() {
+        this.nb_ligne = 7;
+        this.nb_col = 5;
+        this.tab = Array(this.nb_ligne).fill().map(() => Array(this.nb_col).fill(0));
+    }
+
+    get tab() {
+        return this.tab;
+    }
+}
+
+var tab = new Tableau();
+console.log(tab.tab);
+
+
+var pions_rouge = [];
+for (let i = 0; i < nb_col; i++) {
+    pions_rouge.push(new Pion("rouge"));
+}
